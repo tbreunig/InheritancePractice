65bd03be34143967dfdbcbe0f9db91f0fcbea8e0
Tyler <Tyler@10.30.10.155>
2014-01-29 20:24:25 -0600

    Added Javadocs to concrete employee class

--- src/myconcrete/Employee.java
+++ src/myconcrete/Employee.java
@@ -1,14 +1,27 @@
 package myconcrete;
 /**
+ * Concrete Version of Employee 
+ * 
  * @author Tyler
  */
 public class Employee {
     
+    /**
+     * Properties that all employees have.
+     */
     private String fName;
     private String lName;
     private String employeeID;
     private String department;
 
+    /**
+     * Constructor for Concrete Employee
+     * 
+     * @param fName - Parameter holds employee first name
+     * @param lName - Parameter holds employee last name
+     * @param employeeID - Parameter holds employeeID number
+     * @param department - Parameter holds department name
+     */
     public Employee(String fName, String lName, String employeeID, String department) {
         this.fName = fName;
         this.lName = lName;
@@ -16,34 +29,74 @@ public class Employee {
         this.department = department;
     }
 
+    /**
+     * Gets employees first name.
+     * 
+     * @return - first name as a String
+     */
     public String getfName() {
         return fName;
     }
 
+    /**
+     * Sets the employee name in the form of a string object.
+     * 
+     * @param fName - a string object 
+     */
     public void setfName(String fName) {
         this.fName = fName;
     }
 
+    /**
+     * Gets the employee last name in the form of a string object.
+     * 
+     * @return - a string object 
+     */
     public String getlName() {
         return lName;
     }
 
+    /**
+     * Gets the employee first name in the form of a string object.
+     * 
+     * @param lName - a string object
+     */
     public void setlName(String lName) {
         this.lName = lName;
     }
 
+    /**
+     * Gets the employee ID in the form of a string object.
+     * 
+     * @return - a string object
+     */
     public String getEmployeeID() {
         return employeeID;
     }
 
+    /**
+     * Sets the employee ID in the form of a string object.
+     * 
+     * @param employeeID - a string object
+     */
     public void setEmployeeID(String employeeID) {
         this.employeeID = employeeID;
     }
 
+    /**
+     * Gets the employee department in the form of a string object
+     * 
+     * @return - a string object
+     */
     public String getDepartment() {
         return department;
     }
 
+    /**
+     * Sets the employee department in the form of a string object.
+     * 
+     * @param department - a string object
+     */
     public void setDepartment(String department) {
         this.department = department;
     }
